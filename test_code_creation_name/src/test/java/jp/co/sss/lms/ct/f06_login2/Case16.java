1ff72dbd5cd1f175e99d6bb4d45a11607fab0a87
h_taguchi <h_taguchi@braingate.co.jp>
2026-06-23 17:45:44 +0900

    fix:Hibiki Case16テスト1～3記述
    テストクラスのみ記述
    他クラスの未実装部分を呼び出している箇所はエラーになっている

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case16.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case16.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+
+import jp.co.sss.lms.ct.f01_login1.LoginPage;
+import jp.co.sss.lms.util.Constants;
 
 /**
  * 結合テスト ログイン機能②
@@ -19,10 +23,16 @@ import org.junit.jupiter.api.TestMethodOrder;
 @DisplayName("ケース16 受講生 初回ログイン 変更パスワード未入力")
 public class Case16 {
 
+	private static LoginPage loginPage;
+	
+	private static AgreeSecurityPage agreeSecurityPage;
+	
 	/** 前処理 */
 	@BeforeAll
 	static void before() {
 		createDriver();
+		loginPage = new LoginPage(webDriver);
+		agreeSecurityPage = new AgreeSecurityPage(webDriver);
 	}
 
 	/** 後処理 */
@@ -35,21 +45,36 @@ public class Case16 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		
+		assertEquals(Constants.TRUE_TITLE_LOGIN, webDriver.getTitle());
+		assertEquals(Constants.TRUE_H2_LOGIN, loginPage.getH2());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		loginPage.tryLogin(Constants.TRUE_LOGIN_ID, Constants.TRUE_PASSWORD_DEFAULT);
+		
+		assertEquals(Constants.TRUE_TITLE_AGREE_SECURITY, webDriver.getTitle());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
-		// TODO ここに追加
+		agreeSecurityPage.checkAgree();
+		agreeSecurityPage.clickNextBtn();
+		
+		assertEquals(Constants.TRUE_TITLE_LOGIN, webDriver.getTitle());
+		assertEquals(Constants.TRUE_H2_LOGIN, loginPage.getH2());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
