dcce149807a726b83dcc7530518ed8b94bd5fdde
h_taguchi <h_taguchi@braingate.co.jp>
2026-06-24 13:48:38 +0900

    fix:Hibiki ケース17完成
    テスト1～3はケース16と共通
    テスト4もケース16のテスト5を定数を変え、アサーションで比較するものをページタイトルに変えただけで実装できた

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+
+import jp.co.sss.lms.ct.f01_login1.CourseDetailPage;
+import jp.co.sss.lms.ct.f01_login1.LoginPage;
+import jp.co.sss.lms.util.Constants;
 
 /**
  * 結合テスト ログイン機能②
@@ -19,10 +24,22 @@ import org.junit.jupiter.api.TestMethodOrder;
 @DisplayName("ケース17 受講生 初回ログイン 正常系")
 public class Case17 {
 
+	private static LoginPage loginPage;
+	
+	private static AgreeSecurityPage agreeSecurityPage;
+	
+	private static ChangePasswordPage changePasswordPage;
+	
+	private static CourseDetailPage courseDetailPage;
+	
 	/** 前処理 */
 	@BeforeAll
 	static void before() {
 		createDriver();
+		loginPage = new LoginPage(webDriver);
+		agreeSecurityPage = new AgreeSecurityPage(webDriver);
+		changePasswordPage = new ChangePasswordPage(webDriver);
+		courseDetailPage = new CourseDetailPage(webDriver);
 	}
 
 	/** 後処理 */
@@ -35,28 +52,50 @@ public class Case17 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		
+		assertEquals(Constants.TRUE_TITLE_LOGIN, webDriver.getTitle());
+		assertEquals(Constants.TRUE_H2_LOGIN, loginPage.getH2());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		loginPage.tryLogin(Constants.TRUE_LOGIN_ID, Constants.TRUE_PASSWORD_DEFAULT);
+		
+		assertEquals(Constants.TRUE_TITLE_AGREE_SECURITY, webDriver.getTitle());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
-		// TODO ここに追加
+		agreeSecurityPage.checkAgree();
+		agreeSecurityPage.clickNextBtn();
+		
+		assertEquals(Constants.TRUE_TITLE_CHANGE_PASSWORD, webDriver.getTitle());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 変更パスワードを入力し「変更」ボタン押下")
 	void test04() {
-		// TODO ここに追加
+		changePasswordPage.inputCurrentPasword(Constants.TRUE_PASSWORD_DEFAULT);
+		changePasswordPage.inputNewPassword(Constants.TRUE_PASSWORD);
+		changePasswordPage.inputComfirmPassword(Constants.TRUE_PASSWORD);
+		changePasswordPage.clickChangeBtn();
+		changePasswordPage.ckickChangeBtnInMordalWindow();
+		
+		assertEquals(Constants.TRUE_TITLE_COURCE_DETAIL, webDriver.getTitle());
+		
+		getEvidence(new Object(){});
 	}
 
 }
