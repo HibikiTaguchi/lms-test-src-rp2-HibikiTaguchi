879c762c50bfce64834e397806a8829ac3bd3631
h_taguchi <h_taguchi@braingate.co.jp>
2026-06-25 13:07:15 +0900

    fix:Hibiki Case10テスト1～3記述
    テストクラスのみ記述
    他クラスのメンバは未実装

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +11,10 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import jp.co.sss.lms.ct.f01_login1.CourseDetailPage;
+import jp.co.sss.lms.ct.f01_login1.LoginPage;
+import jp.co.sss.lms.util.Constants;
+
 /**
  * 結合テスト 勤怠管理機能
  * ケース10
@@ -18,11 +23,17 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース10 受講生 勤怠登録 正常系")
 public class Case10 {
-
+	
+	private static LoginPage loginPage;
+	
+	private static CourseDetailPage courseDetailPage;
+	
 	/** 前処理 */
 	@BeforeAll
 	static void before() {
 		createDriver();
+		loginPage = new LoginPage(webDriver);
+		courseDetailPage = new CourseDetailPage(webDriver);
 	}
 
 	/** 後処理 */
@@ -35,21 +46,34 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		
+		assertEquals(Constants.TRUE_TITLE_LOGIN, webDriver.getTitle());
+		assertEquals(Constants.TRUE_H2_LOGIN, loginPage.getH2());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(2)
-	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
+	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		loginPage.tryLogin(Constants.TRUE_LOGIN_ID, Constants.TRUE_PASSWORD_DEFAULT);
+		
+		assertEquals(Constants.TRUE_TITLE_AGREE_SECURITY, webDriver.getTitle());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		courseDetailPage.clickAttendance();
+		
+		assertEquals(Constants.TRUE_TITLE_ATTENDANCE_INFO_MANAGEMENT, webDriver.getTitle());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
